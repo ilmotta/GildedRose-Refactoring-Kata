64b85c9f176aa34eff07421014291052d8da4082
ilmotta <icaro.ldm@gmail.com>
2016-04-14 06:38:50 -0300

    Move Item class to its own file.

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,3 +1,5 @@
+require_relative 'item'
+
 class GildedRose
   MIN_QUALITY = 0
   MAX_QUALITY = 50
@@ -52,19 +54,5 @@ class GildedRose
         end
       end
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
