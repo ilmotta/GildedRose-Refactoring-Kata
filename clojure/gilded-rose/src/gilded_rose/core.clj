6ea47ba264b9cf2521a990b6edbe143296197f0b
ilmotta <icaro.ldm@gmail.com>
2016-12-02 11:25:34 -0200

    Extract a few generic functions to evolve items

--- clojure/gilded-rose/src/gilded_rose/core.clj
+++ clojure/gilded-rose/src/gilded_rose/core.clj
@@ -1,6 +1,28 @@
 (ns gilded-rose.core
   (:gen-class))
 
+(defn indestructible?
+  [name]
+  (= name "Sulfuras, Hand of Ragnaros"))
+
+(defn aged-brie?
+  [name]
+  (= name "Aged Brie"))
+
+(defn evolve-item
+  [{:keys [name sell-in quality] :as item}]
+  (cond
+    (aged-brie? name) {:quality #(min 50 (inc %)) :sell-in dec}
+    (indestructible? name) {:quality identity :sell-in identity}
+    :else {:quality identity :sell-in identity}))
+
+(defn evolve-items
+  [items]
+  (map (fn [item]
+         (update (update item :quality (:quality (evolve-item item)))
+                 :sell-in (:sell-in (evolve-item item))))
+        items))
+
 (defn log [step]
   (fn [item] (do
                (println (str "step-" step)  item)
--- clojure/gilded-rose/test/gilded_rose/core_test.clj
+++ clojure/gilded-rose/test/gilded_rose/core_test.clj
@@ -26,6 +26,17 @@
   (expect 10
           (:quality (first (update-quality (update-quality items))))))
 
+; Sulfuras item
+; =============
+(let [items [{:name "Sulfuras, Hand of Ragnaros" :sell-in 20 :quality 10}]]
+  ; It never gets old
+  (expect 20
+          (:sell-in (first (evolve-items (evolve-items items)))))
+
+  ; It never decreases in quality
+  (expect 10
+          (:quality (first (evolve-items (evolve-items items))))))
+
 (defn expect-to-increase-quality
   [item]
   (do
@@ -37,6 +48,17 @@
     (expect 50
             (:quality (first (update-quality [(assoc item :quality 50)]))))))
 
+(defn new-expect-to-increase-quality
+  [item]
+  (do
+    ; It increases the quality the older it gets
+    (expect (+ (:quality item) 2)
+            (:quality (first (evolve-items (evolve-items [item])))))
+
+    ; It caps the quality to a maximum
+    (expect 50
+            (:quality (first (evolve-items [(assoc item :quality 50)]))))))
+
 ; "Aged Brie" item
 (let [item {:name "Aged Brie" :sell-in 20 :quality 0}]
   (expect-to-increase-quality item)
@@ -46,6 +68,16 @@
           (:sell-in (first (update-quality [(assoc item :sell-in 0)]))))
   (expect 19
           (:sell-in (first (update-quality [item])))))
+
+; "Aged Brie" item
+(let [item {:name "Aged Brie" :sell-in 20 :quality 0}]
+  (new-expect-to-increase-quality item)
+  (expect -2
+          (:sell-in (first (evolve-items [(assoc item :sell-in -1)]))))
+  (expect -1
+          (:sell-in (first (evolve-items [(assoc item :sell-in 0)]))))
+  (expect 19
+          (:sell-in (first (evolve-items [item])))))
 
 ; "Backstage passes..." item
 (let [item {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 20 :quality 20}]
